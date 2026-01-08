31e23b737dc630a9e5e45c75dfa48c1850d1b40e
srxshiv <srxshiv@gmail.com>
2026-01-09 03:11:58 +0530

    added all orders get with query param

--- src/controllers/getTrades.ts
+++ src/controllers/getTrades.ts
@@ -2,7 +2,12 @@ import { Request, Response } from 'express';
 import { getDB } from '../config/db'; 
 
 export const getTrades = async (req: Request, res: Response) => {
-    const db = getDB();
+  const db = getDB();
+    const query = req.query.all
+    if(query){
+      const trades = await db.all(`SELECT * FROM orders ORDER BY timestamp DESC`);
+    res.json(trades);
+    }
     const trades = await db.all(`SELECT * FROM orders WHERE status = 'EXECUTED' ORDER BY timestamp DESC`);
     res.json(trades);
   };
